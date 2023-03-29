4e3a1404d7e1fbe669e2e1a8c0846bac997a607d
Inna Cervenkova <inna.cervenkova@gmail.com>
2023-03-30 00:55:46 +0200

    Cviceni na podminky

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ Vytvořte objekt person, do kterého vložte uživatelem zadané údaje. Objekt 
 Poté, co uživatel zadá všechny údaje, vypište do stránky objekt person v nějakém hezkém formátu a zkontrolujte, že obsahuje správné informace.
 */
 
+/*
 const person = {
   fullName: prompt('Zadej sve jmeno'),
   age: Number(prompt('Zadej svuj vek')),
@@ -16,7 +17,7 @@ const person = {
 
 document.body.innerHTML += '<p> Jmeno: ' + person.fullName + '</p>'
 document.body.innerHTML += '<p> Vek: ' + person.age + '</p>'
-
+*/
 
 
 /****************ZADANI 2.********************/
@@ -34,6 +35,31 @@ if (vek >= 65) {
   document.body.innerHTML += 'Nízký věk'
 }
 */
+
+/****************ZADANI 3.********************/
+/*
+Registrace na očkování
+
+Nejdříve nechte uživatele zadat všechny hodnoty, tedy jméno, věk i heslo. Uložte si je do dobře pojmenovaných proměnných.
+Napište první podmínku, ve které zkontrolujte, že věk uživatele je větší nebo roven 65. Pokud ano, vypište do stránky „věk v pořádku“. Pokud uživateli není alespoň 65 let, vypište „nízký věk“.
+Napište druhou podmínku, která zkontroluje, zda je zadané heslo delší než osm znaků. Pokud není, vypište „slabé heslo“. Heslo se bude kontrolovat pouze v případě, kdy uživatel splnil první podmínku (věk alespoň 65 let).
+*/
+
+const fullName = prompt('Zadej cele jmeno:');
+const age = Number(prompt('Zadej svuj vek:'));
+const password = prompt('Zadej heslo:');
+
+if (age >= 65) {
+  document.body.innerHTML += `<p>Vek je v poradku</p>`
+  if (password.length <= 8) {
+    document.body.innerHTML += `<p>Slabe heslo</p>`
+  } else {
+    document.body.innerHTML += `<p> Heslo je v poradku</p>`
+  }
+} else {
+  document.body.innerHTML += `<p>Nizky vek</p>`
+}
+
 
 
 
